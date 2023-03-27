4495c3d517a3b0aa23368f289a2bc87f8052724d
Ruben00alex <ruben.00alex@gmail.com>
2023-03-26 21:01:00 -0700

    commit fb-01

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,7 +7,7 @@
         src="@/assets/logo.svg"
       />
 
-      <div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
+      <div class="text-body-2 font-weight-light mb-n1">Welctest2ome to</div>
 
       <h1 class="text-h2 font-weight-bold">TESTING</h1>
 
